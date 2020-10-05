7759bfd12610d7cdd75a0db48862f5c9b44afd69
Frank Carmona <frankcarmonavasquez@gmail.com>
2020-10-05 12:46:15 +0200

    Configurando multiples puntos de entrada

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname),
-        filename: 'bundle.js'
-    }
-}
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js",
+  },
+};
