cc02a81f6e9ca7f25613133039349b68b3279478
Frank Carmona <frankcarmonavasquez@gmail.com>
2020-10-13 20:47:19 +0200

    pruebas con react en webpack

---
+++ react/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const MiniCSSExtracPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "js moderno",
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
